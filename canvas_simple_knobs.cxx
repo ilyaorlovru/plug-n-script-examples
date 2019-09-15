c8f63193366b339f536989754e3b42e44a4a0ec7
Ilya <ilya_orlov@mail.ru>
2019-09-15 14:16:55 +0300

    Keep things more clear

---
+++ canvas_simple_knobs.cxx
@@ -0,0 +1,14 @@
+//////////////////////////////////////
+// Very simple knobs
+// written by Ilya Orlov (LetiMix) 
+// https://www.youtube.com/channel/UCbS0EpAZytfizVvysUluEHQ
+// St. Petersburg, Russia, 2019
+//////////////////////////////////////
+
+// THE MAIN SCRIPT IS IN KUIML FILE
+
+////////////////////////////
+// script name
+
+string name="Very simple knobs";
+
